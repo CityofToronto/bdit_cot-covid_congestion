4023a93156f4445b0834e92e481472e40af05e57
katiRG <cnangini@gmail.com>
2020-03-27 15:53:26 -0400

    WIP: set up tti-summary lineChart

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,3 +22,37 @@ $(function () {
 
 // -----------------------------------------------------------------------------
 // CUSTOM CODE
+
+
+function uiHandler(event) {
+  // if (event.target.id === "vkt-menu") {
+  //   ptcvolTOD = event.target.value; // "All day" initially
+  //   showVktMap();
+  //   updateTitles();
+  // }
+
+}
+
+// -----------------------------------------------------------------------------
+$(document).ready(function(){
+  // ---------------------------------------------------------------------------
+  // Chart SVGs
+  // Fig 1 - TTI Summary lineChart
+  // ttiSvg = d3.select(".tpdline")
+  //   .append("svg")
+  //   .attr("id", "ttiSummary");
+
+  // Initial page load
+  i18n.load(["/* @echo SRC_PATH *//i18n"], () => {
+    settTtiLine.x.label = i18next.t("x_label", {ns: "tti-summary"}),
+    settTtiLine.y.label = i18next.t("y_label", {ns: "tti-summary"}),
+    d3.queue()
+      .defer(d3.json, "/* @echo SRC_PATH *//data/fig1_tti_summary.json") // Fig 1 - daily trip lineChart in city
+      .await(function(error, fig1) {
+        // Load data files into objects
+
+      });
+  })
+})
+
+$(document).on("change", uiHandler);
